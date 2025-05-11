5b83bf6eb85a8610d963d6b5f96e2114a79506c0
MykolaichukIhor <mykolaichuk.ihor@lll.kpi.ua>
2025-05-11 11:46:55 +0300

    Updated checking.h
    added struct Point and class Rectangle

--- checking.h
+++ checking.h
@@ -1,8 +1,46 @@
 #ifndef CHECKING_H
 #define CHECKING_H
 
+struct Point {
+    int x, y;
+    bool isTop;    // true для верхніх точок, false для нижніх
+    bool isLeft;   // true для лівих точок, false для правих
+};
 
 
+class Rectangle {
+
+private: 
+ Point topLeft, bottomRight;
+ Point topRight, bottomLeft;
+
+
+  void calculateOtherPoints() {
+
+
+
+topRight.x = bottomRight.x;
+        topRight.y = topLeft.y;
+        topRight.isTop = true;
+        topRight.isLeft = false;
+        
+        bottomLeft.x = topLeft.x;
+        bottomLeft.y = bottomRight.y;
+        bottomLeft.isTop = false;
+        bottomLeft.isLeft = true;
+
+
+
+
+
+  }
+
+
+
+
+
+
+};
 
 
 

8140c75a9f7020d975d91c96846cddb4d46a9e30
MykolaichukIhor <mykolaichuk.ihor@lll.kpi.ua>
2025-05-11 12:43:59 +0300

    updated main

--- task43.cc
+++ task43.cc
@@ -8,11 +8,48 @@ class RectangleWrapper {
 
     public:
      int value;
-     
+     RectangleWrapper(int val = 0) : value(val) {}
+
+     bool isPrime() const {
+        if (value <= 1) return false;
+        for (int i = 2; i * i <= value; ++i) {
+            if (value % i == 0) return false;
+        }
+        return true;
+    }
 
 
 
 };
+
+
+void printMatrix(const vector<vector<string>>& matrix) {
+    for (const auto& row : matrix) {
+        for (const auto& elem : row) {
+            cout << elem << "\t";
+        }
+        cout << endl;
+    }
+}
+
+
+int findMaxNumber(const vector<vector<string>>& matrix) {
+    int maxNum = INT_MIN;
+    for (const auto& row : matrix) {
+        for (const auto& elem : row) {
+            try {
+                int num = stoi(elem);
+                if (num > maxNum) {
+                    maxNum = num;
+                }
+            } catch (const invalid_argument&) {
+                
+                continue;
+            }
+        }
+    }
+    return maxNum;
+}
 
 
 int main() {

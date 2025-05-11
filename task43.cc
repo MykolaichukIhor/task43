cfc851a1c9594bfbf53bb8e21011093b2829fb35
MykolaichukIhor <mykolaichuk.ihor@lll.kpi.ua>
2025-05-11 13:03:03 +0300

    Update task43.cc

--- task43.cc
+++ task43.cc
@@ -30,7 +30,17 @@ class RectangleWrapper {
 
 class Solution {
 
-
+public:
+    vector<RectangleWrapper> rectangles;
+    vector<vector<string>> current_matrix;
+    
+    void print() const {
+        for (size_t i = 0; i < rectangles.size(); ++i) {
+            rectangles[i].print();
+            if (i != rectangles.size() - 1) cout << " -> ";
+        }
+        cout << "\n";
+    }
 
 
 };
